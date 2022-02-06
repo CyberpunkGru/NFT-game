322e72e535d6a826d98428a1dba3212f71ccab0d
CyberpunkGru <grudev325@gmail.com>
2022-02-06 06:47:45 -0800

    add constructor function

--- contracts/core/CicadaToken.sol
+++ contracts/core/CicadaToken.sol
@@ -10,4 +10,15 @@ contract StandardToken is ERC20, ERC223 {
     string internal _symbol;
     uint8 internal _decimals;
     uint256 internal _totalSupply;
+
+    mapping (address => uint256) internal balances;
+    mapping (address => mapping (address => uint256)) internal allowed;
+
+    constructor(string memory name, string memory symbol, uint8 decimals, uint256 totalSupply) public {
+        _symbol = symbol;
+        _name = name;
+        _decimals = decimals;
+        _totalSupply = totalSupply;
+        balances[msg.sender] = totalSupply;
+    }
 }
