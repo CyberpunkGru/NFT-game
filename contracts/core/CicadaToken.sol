64cb07152c583e0f200d11c6110457de79f5591d
MiracleCorunus <miraclegwon@gmail.com>
2022-02-06 06:41:47 -0800

    add variables

--- contracts/core/CicadaToken.sol
+++ contracts/core/CicadaToken.sol
@@ -1,9 +1,13 @@
-pragma solidity ^0.5 <0.6.0;
+pragma solidity >=0.5.0 <0.6.0-0 <0.6.0;
 
-import '../Tools/SafeMath.sol';
-import '../Tools/ERCInterfaces.sol';
+import "../Tools/SafeMath.sol";
+import "../Tools/ERCInterfaces.sol";
 
 contract StandardToken is ERC20, ERC223 {
-  using SafeMath for uint;
+    using SafeMath for uint256;
 
+    string internal _name;
+    string internal _symbol;
+    uint8 internal _decimals;
+    uint256 internal _totalSupply;
 }
