60df6444df080c9b61fffdf84cc237b8d9e5d6b1
CyberpunkGru <grudev325@gmail.com>
2022-03-08 12:16:39 -0800

    add balance of and transferfrom

--- contracts/core/CicadaToken.sol
+++ contracts/core/CicadaToken.sol
@@ -49,12 +49,29 @@ contract StandardToken is ERC20, ERC223 {
         returns (uint256) {
         return _totalSupply;
     }
+    
     function transfer(address _to, uint256 _value) public returns (bool) {
      require(_to != address(0), "Can not send to null");
      require(_value <= balances[msg.sender], "Can not send more than balance");
      balances[msg.sender] = SafeMath.sub(balances[msg.sender], _value);
      balances[_to] = SafeMath.add(balances[_to], _value);
      emit Transfer(msg.sender, _to, _value);
+     return true;
+   }
+
+   function balanceOf(address _owner) public view returns (uint256 balance) {
+    return balances[_owner];
+   }
+
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+    require(_to != address(0), "Can not send to null");
+     require(_value <= balances[_from], "Can not send more than balance");
+     require(_value <= allowed[_from][msg.sender], "Can not send more than allowed");
+
+      balances[_from] = SafeMath.sub(balances[_from], _value);
+     balances[_to] = SafeMath.add(balances[_to], _value);
+     allowed[_from][msg.sender] = SafeMath.sub(allowed[_from][msg.sender], _value);
+     emit Transfer(_from, _to, _value);
      return true;
    }
 }
